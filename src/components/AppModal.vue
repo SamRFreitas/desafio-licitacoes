2918cf839ff5e432f2088cc250b290dde0605766
samrfreitas <samuelrfreitassi@gmail.com>
2023-03-10 18:51:57 -0300

    feature: AppModal

---
+++ src/components/AppModal.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    v-if="visible"
+    class="modal app-modal"
+    tabindex="-1"
+    style="display: block;"
+  >
+    <div class="modal-dialog  modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title text-center w-100">
+            {{ modalTitle }}
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+            @click="closeModal()"
+          />
+        </div>
+        <div class="modal-body">
+          <slot />
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+            @click="closeModal()"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    visible: Boolean,
+    modalTitle: String,
+  },
+  emits: ['onCloseModal'],
+  setup (props, {emit }) {
+    
+    function closeModal () {
+      emit('onCloseModal', false)
+    }
+
+    return {
+      closeModal
+    }
+  }
+}
+</script>
