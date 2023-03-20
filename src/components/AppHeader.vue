1daa5bd15e1123b2fe5a078790926829b0213b1e
samrfreitas <samuelrfreitassi@gmail.com>
2023-03-20 17:31:51 -0300

    feature: add header to application

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar header-color shadow-lg d-flex justify-content-center">
+    <div class="d-flex align-items-center">
+      <BaseIcon name="logo" />
+      <a class="navbar-brand fs-3 text-white ms-2">effecti</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon.vue'
+
+export default {
+  components: {
+    BaseIcon
+  }
+}
+</script>
