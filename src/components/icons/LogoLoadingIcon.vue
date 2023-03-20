9c6c39c760b2746fff7169ba87deeb931056f550
samrfreitas <samuelrfreitassi@gmail.com>
2023-03-20 17:39:25 -0300

    feature: add icon component

---
+++ src/components/icons/LogoLoadingIcon.vue
@@ -0,0 +1,80 @@
+<template>
+  <svg
+    id="eUxN5K1vmF31"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 1000 1000"
+    shape-rendering="geometricPrecision"
+    text-rendering="geometricPrecision"
+  >
+
+    <g
+      id="eUxN5K1vmF33_tr"
+      transform="translate(500,500) rotate(0)"
+    ><g transform="scale(6.7284,6.7284) translate(0,0)"><g><g transform="matrix(2.7148 0 0 2.7148 0.1598-37.6004)"><polygon
+      points="18.1,-4.5 2.3,4.7 2.3,-11.4 -2.4,-11.4 -2.4,4.7 -18.2,-4.5 -20.5,-0.5 0,11.4 20.5,-0.5"
+    /></g><g transform="matrix(2.7148 0 0 2.7148-37.3045 20.3608)"><polygon
+      points="-11.4,-13.75 4.5,-4.55 -9.5,3.45 -7.1,7.45 6.8,-0.55 6.8,17.75 11.4,17.75 11.4,-5.95 -9.1,-17.75"
+    /></g><g transform="matrix(2.7148 0 0 2.7148 37.3529 20.3608)"><polygon
+      points="11.4,-13.75 9.1,-17.75 -11.4,-5.95 -11.4,17.75 -6.8,17.75 -6.8,-0.55 7.2,7.45 9.5,3.45 -4.4,-4.55"
+    /></g><g transform="matrix(1.1462-2.461 2.461 1.1462 26.1874-55.6236)"><rect
+      width="5.2"
+      height="4.6"
+      rx="0"
+      ry="0"
+      transform="translate(-2.6-2.3)"
+    /></g><g transform="matrix(2.461-1.1462 1.1462 2.461-26.1983-55.5549)"><rect
+      width="4.6"
+      height="5.2"
+      rx="0"
+      ry="0"
+      transform="translate(-2.3-2.6)"
+    /></g><g transform="matrix(2.2231-1.5582 1.5582 2.2231 35.4933 51.5213)"><rect
+      width="4.6"
+      height="5.2"
+      rx="0"
+      ry="0"
+      transform="translate(-2.3-2.6)"
+    /></g><g transform="matrix(1.5584-2.223 2.223 1.5584-35.8098 51.3882)"><rect
+      width="5.2"
+      height="4.6"
+      rx="0"
+      ry="0"
+      transform="translate(-2.6-2.3)"
+    /></g><g transform="matrix(2.7044-.2372 0.2372 2.7044-61.7397 6.0988)"><rect
+      width="5.2"
+      height="4.6"
+      rx="0"
+      ry="0"
+      transform="translate(-2.6-2.3)"
+    /></g><g transform="matrix(.2376-2.7044 2.7044 0.2376 61.7386 6.1884)"><rect
+      width="4.6"
+      height="5.2"
+      rx="0"
+      ry="0"
+      transform="translate(-2.3-2.6)"
+    /></g></g></g></g></svg>
+</template>
+
+<script>
+export default {
+  name: 'LogoLoading'
+}
+</script>
+
+<style>
+
+#eUxN5K1vmF33_tr {
+  animation: eUxN5K1vmF33_tr__tr 3000ms linear infinite normal forwards
+}
+
+@keyframes eUxN5K1vmF33_tr__tr { 
+  0% {
+    transform: translate(500px,500px) rotate(0deg)
+  } 
+  100% {
+    transform: translate(500px,500px) rotate(359.999994deg)
+  }
+}
+
+ </style>
