f59e8e777ff04c911c8b51deb56a3282e7d5ea9d
samrfreitas <samuelrfreitassi@gmail.com>
2023-03-20 17:31:42 -0300

    feature: add icons

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <component
+    :is="iconComponent"
+    width="35"
+    height="35"
+  />  
+</template>
+
+<script>
+const icons = {
+  logo: require('./icons/LogoIcon.vue').default,
+  logoLoading: require('./icons/LogoLoadingIcon.vue').default
+}
+
+export default {
+  name: 'BaseIcon',
+  props: {
+    name: {
+      type: String,
+      required: true,
+      validator(value) {
+        return Object.prototype.hasOwnProperty.call(value, icons)
+      }
+    }
+  },
+  computed: {
+    iconComponent () {
+      return icons[this.name]
+    }
+  }
+}
+</script>
