ab5daff3e093ec91576677ccca4d5e91f5d75a26
samrfreitas <samuelrfreitassi@gmail.com>
2023-03-21 17:15:26 -0300

    feature: add action buttons to rows to show a modal and access the link of licitação

--- src/components/Licitacoes.vue
+++ src/components/Licitacoes.vue
@@ -1,5 +1,6 @@
 <template>
   <vue-good-table
+    class="px-4 mt-4"
     :columns="state.columns"
     :rows="rows"
     :search-options="{
@@ -19,20 +20,58 @@
       pageLabel: 'página',
       rowsPerPageLabel: 'Licitações por página',
     }"
-  >
+  > 
+    <template #table-row="props">
+      <div
+        v-if="props.column.field == 'actions'"
+        class="d-flex justify-content-center"
+      >
+        <button
+          @click="openModal(props)"
+          class="effecti-button"
+        >
+          Detalhes
+        </button>
+
+        <a
+          class="d-flex align-items-center btn btn-danger ms-2"
+          :href="props.row.anexoLink"
+          target="_blank"
+        >
+          <fa icon="link" />
+        </a>
+      </div>
+
+      <span
+        v-if="props.column.field == 'situacaoDoProcesso'"
+        class="badge"
+        :class="checkLicitacaoStatusStyle(props)"
+      >
+        {{ props.row.situacaoDoProcesso }}
+      </span>
+    </template>
     <template #emptystate>
       <p class="text-center">
         Nenhuma Licitação encontrada
       </p>
     </template>
   </vue-good-table>
+
+  <AppModal
+    :visible="state.isModalOpen"
+    :modal-title="state.modalTitle"
+    @on-close-modal="state.isModalOpen = false"
+  > 
+    <LicitacoesDetails :licitacao="state.licitacao" />
+  </AppModal>
 </template>
 
 <script>
 import 'vue-good-table-next/dist/vue-good-table-next.css'
 import { VueGoodTable } from 'vue-good-table-next'
 import {reactive} from 'vue'
-
+import AppModal from './AppModal.vue'
+import LicitacoesDetails from './LicitacoesDetails.vue'
 export default {
   props: {
     rows: {
@@ -41,8 +80,10 @@ export default {
     }
   },
   components: {
-    VueGoodTable
-  },
+    VueGoodTable,
+    AppModal,
+    LicitacoesDetails,
+},
   setup () {
 
     function padTo2Digits(num) {
@@ -64,28 +105,57 @@ export default {
       }
       
     }
+
     const state = reactive({
-      modalIsOpen: false,
+      isModalOpen: false,
+      licitacao: null,
+      modalTitle: null,
       columns: [
       { label: 'Unidade Gestora', field: 'unidadeGestora' },
       { label: 'Processo ADM', field: 'numeroDoProcessoAdm' },
       { label: 'Processo', field: 'numeroDoProcesso' },
       { label: 'Modalidade', field: 'modalidade' },
-      { label: 'Numero Modalidade', field: 'numeroDaModalidade' },
       { label: 'Tipo', field: 'tipo' },
       { label: 'Situação', field: 'situacaoDoProcesso' },
       { label: 'Julgamento', field: 'dataDeJulgamento',  type: 'date', formatFn: formatDate},
       { label: 'Homologação', field: 'dataDeHomologacao', type: 'date', formatFn: formatDate},
-      { label: 'Objeto', field: 'objeto' },
-      { label: 'Valor', field: 'valor' },
-      // { label: 'Anexo', field: 'possuiAnexo' },
+      { label: 'Ações', field: 'actions' },
       ],
       options: {
         lengthMenu: [5, 10]
       }
     })
 
-    return { state }
+    function checkLicitacaoStatusStyle (props) {
+      if (props.row.situacaoDoProcesso === 'Aberta') {
+        return 'text-bg-info'
+      }
+
+      if (props.row.situacaoDoProcesso === 'Aguardando abertura') {
+        return 'text-bg-warning'
+      }
+
+      if (props.row.situacaoDoProcesso === 'Concluída') {
+        return 'text-bg-success'
+      }
+
+      if (props.row.situacaoDoProcesso === 'Revogada') {
+        return 'text-bg-dark'
+      }
+
+      if (props.row.situacaoDoProcesso === 'Deserta') {
+        return 'text-bg-danger'
+      }
+     
+    }
+
+    function openModal (props) {
+      state.licitacao = props.row
+      state.modalTitle = state.licitacao.unidadeGestora
+      state.isModalOpen = true
+    }
+
+    return { state, openModal, checkLicitacaoStatusStyle }
   }
 }
 </script>
